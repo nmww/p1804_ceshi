8c209e7ce4c203dd65a77d3a06f3f4dbe2eec52d
nmww <2423550286@qq.com>
2018-05-11 10:02:22 +0800

    caiquan while

---
+++ p1804/p09/whileT.py
@@ -0,0 +1,20 @@
+
+b = 10
+
+# while 循环条件:
+#   循环语句
+
+while b > 6:
+    print (" nihao mv ")
+    b = b-1  # 9
+
+#while True:  # True  False
+#    print (" nihao mv ")
+#    print (" nihao shuai guo ")
+
+
+#1. while 循环结束
+#2. while 循环条件 不成立
+print ("=======================")
+
+
---
+++ p1804/p09/whileTest.py
@@ -0,0 +1,26 @@
+import random
+
+a = random.randint(1,9)
+print ("随机的数字是 %d " % (a) )
+
+b = 10
+
+if b > 20:  # True 成立  False 不成立
+    pass
+else:
+    pass
+
+print ("-------------")
+
+# while 下方相同缩进范围内的,所有语句
+while False: #不会进入 循环
+    print ("")
+    print ("")
+    print ("")
+    print ("")
+    print ("")
+    print ("")
+    pass
+
+print ("---------")
+
