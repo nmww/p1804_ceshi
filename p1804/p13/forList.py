77f8cd81f73a982010342c9a056ad497d4c9d1a5
nmww <2423550286@qq.com>
2018-05-17 09:18:16 +0800

    mingp

--- p1804/p13/forList.py
+++ p1804/p13/forList.py
@@ -1,6 +1,9 @@
 
 numList = [1,21,3,4,5,76,2,23,12,3211]
 
+print ('3号位置是：%d \t 8号位置是：%d。'% (numList[3], numList[8]))
+
+
 for i in numList: # 遍历 列表
     print (i)
 
--- p1804/p13/mingpian.py
+++ p1804/p13/mingpian.py
@@ -1,5 +1,14 @@
 
 
+
+list_card = [] # 空列表，用于保存名片dic
+'''
+dic
+company : 北财科技
+name : 张三
+手机号： 110
+'''
+
 def systemMenu():
     ''' 名片管理系统，功能菜单展示'''
     print ('*'*30)
@@ -15,6 +24,15 @@ def tuichu():
     print ("成功退出名片管理系统")
 
 def newCard():
+    print('*'*50)
+    print("开始创建名片")
+    name = input("请输入姓名：")
+    com = input ("请输入公司名:")
+    phone_num = input("请输入手机号:")
+    dic = {'name':name,'company':com,'phone':phone_num}
+    list_card.append(dic)
+    print(list_card)
+    print("名片保存成功，姓名是：%s" % dic['name'])
     pass
 
 def sAll():
