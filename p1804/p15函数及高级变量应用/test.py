89e41f40f02cb75371653e03587d1ee9f0e4d4bb
nmww <2423550286@qq.com>
2018-05-18 11:14:19 +0800

    遍历

--- p1804/p15函数及高级变量应用/test.py
+++ p1804/p15函数及高级变量应用/test.py
@@ -11,10 +11,24 @@ def add(*anything):
 def addsan(a,b,c):
     a+b+c
 
+re = 0
 def add_some(*num):
-    print (num)
+    global re
+    for i in num :
+        re = i+re
+    print (re)
 
 add_some(1,2,3,4,5,6)
+add_some(1,20,300,4,5,6)
+
+#tup = (1,2,3,4,5,6)
+
+# 1- 10
+#a = 0
+#for i in tup:
+#    a = a+i
+
+#print (a)
 
 a = 1
 a = 'a'
