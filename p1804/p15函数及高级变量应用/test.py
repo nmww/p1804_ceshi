e00c2e163fa6439ab7130f959020e21379e97359
nmww <2423550286@qq.com>
2018-05-18 10:35:16 +0800

    ace

---
+++ p1804/p15函数及高级变量应用/test.py
@@ -0,0 +1,25 @@
+
+
+# 型参  比喻成 盒子
+# 变量  比喻成 盒子
+
+def add(a,b):
+    print (a)
+    print (b)
+def add(*anything):
+    print (anything)
+def addsan(a,b,c):
+    a+b+c
+
+def add_some(*num):
+    print (num)
+
+add_some(1,2,3,4,5,6)
+
+a = 1
+a = 'a'
+a = [1]
+a = {}
+a = ()
+
+
