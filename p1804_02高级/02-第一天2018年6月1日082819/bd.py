7b062d8a316bf9bdc8bd367caabcd2f4e932a9d3
nmww <2423550286@qq.com>
2018-06-01 14:30:03 +0800

    file

--- p1804_02高级/02-第一天2018年6月1日082819/bd.py
+++ p1804_02高级/02-第一天2018年6月1日082819/bd.py
@@ -14,11 +14,19 @@ print ('b的内容是 %s'% b)
 f1 = open('1.txt','r')
 c = f1.readlines()
 
-f2 = open('1.txt','w')
+f2 = open('1.txt','w+')
 for i in c:
+    #'abcd\n'
     s = i[:len(i)-1]
     f2.write(s+"*\n")
 
 f1.close()
 f2.close()
+
+print ('文件的名字是 %s' % f2.name)
+print ('文件是否已经关闭 %s '% f2.closed)
+print ('该文件的打开模式是 %s ' % f2.mode)
+
+
+
 
---
+++ p1804_02高级/02-第一天2018年6月1日082819/filei图片读取和保存.py
@@ -0,0 +1,12 @@
+
+# 打开图片并特殊保存
+
+f = open('img61.jpg','rb+')
+cimg = f.read()
+print(cimg)
+f.close()
+
+f = open('2.jpg','wb+')
+f.write(cimg)
+f.close()
+
