a196be659cd83bb8e7f8cf000d91d88f753d3e36
nmww <2423550286@qq.com>
2018-06-19 14:55:59 +0800

    a

---
+++ p1804_02高级/12_飞机/01_创建游戏窗口.py
@@ -0,0 +1,33 @@
+#-*- coding=utf-8 -*-
+import pygame
+
+def main():
+
+    # 创建游戏窗口
+    screen = pygame.display.set_mode((400,500),0,32)
+
+    # 把本地文件夹的图片，获取到 代码中
+    background = pygame.image.load('./images/background.png')
+
+    # 把图片 加载 到 游戏窗口上
+    screen.blit(background, (0,0))
+
+    # 刷新显示
+    pygame.display.update()
+
+    '''
+    # 1.x 2.y 3.宽度 4.高度
+    feiji = pygame.Rect(100,500,50,50)
+    print('x = ',feiji.x)
+    print('y = ',feiji.y)
+    print('width = ', feiji.width)
+    print('height = ', feiji.height)
+    screen = pygame.display.set_mode((480, 700))
+    '''
+    while True:
+        pass
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ p1804_02高级/12_飞机/hero_rect.py
@@ -0,0 +1,13 @@
+#-*- coding=utf-8 -*-
+import pygame
+
+# 1.x 2.y 3.宽度 4.高度
+feiji = pygame.Rect(100,500,50,50)
+print('x = ',feiji.x)
+print('y = ',feiji.y)
+print('width = ', feiji.width)
+print('height = ', feiji.height)
+screen = pygame.display.set_mode((480, 700))
+while True:
+    pass
+
